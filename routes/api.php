2afb199faa581f979ba52f5667416e80bad6f8ab
learning <hurainakhan888@gmail.com>
2026-05-06 11:51:50 +0500

    Add CORS headers and update contact success message

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,11 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit;
+}
 
 use App\Http\Controllers\InquiryController;
 use App\Http\Middleware\CorsMiddleware;
